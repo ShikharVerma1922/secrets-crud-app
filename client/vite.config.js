2ab7a337627c315e7c26a3ad68324935d19d6e69
Shikhar Verma <shikharverma@Shikhars-MacBook-Air.local>
2024-10-06 15:24:28 +0530

    add versel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Change the output directory from 'dist' to 'build'
+    // outDir: "build",
+    outDir: "dist",
   },
 });
